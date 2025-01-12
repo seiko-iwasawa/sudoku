5a3062f5be7e0a8c92cb9add22b675bd6bda8a45
seiko.iwasawa <seiko.iwasawa@gmail.com>
2025-01-12 15:15:29 +0300

    some refactor

--- pysudoku/solverHuman.py
+++ pysudoku/solverHuman.py
@@ -1,5 +1,4 @@
 from collections.abc import Generator
-from copy import deepcopy
 from typing import Literal, override
 
 from pysudoku.analyzer import Analyzer
@@ -32,62 +31,55 @@ class SolverHuman(SolverABC):
 
     def __init__(self, sudoku: Sudoku, explain: bool = False) -> None:
         super().__init__(sudoku)
-        self._complexity: int = 0
         self._explain = explain
+        self._complexity = 0
+
+    def _move(
+        self, cell: Sudoku.Cell, val: Sudoku.Cell.Option, info: str, complexity: int
+    ) -> InfoMove:
+        move = InfoMove(cell, val, info)
+        self._complexity += complexity
+        if self._explain:
+            print(move)
+        return move
 
     def _easy_move(self, analyzer: Analyzer) -> InfoMove | None:
         for cell in self._sudoku.empty_cells:
             if len(analyzer[cell]) == 1:
-                for val in analyzer[cell]:
-                    ...
-                return InfoMove(cell, val, "other options are blocked")
+                val = next(iter(analyzer[cell]))
+                return self._move(cell, val, "other options are blocked", 1)
+
+    def _get_unique_group(
+        self, analyzer: Analyzer, cell: Sudoku.Cell, val: Sudoku.Cell.Option
+    ) -> str | None:
+        if analyzer.row_occ(cell.row, val) == 1:
+            return "row"
+        elif analyzer.col_occ(cell.col, val) == 1:
+            return "col"
+        elif analyzer.block_occ(cell.block, val) == 1:
+            return "block"
 
     def _medium_move(self, analyzer: Analyzer) -> InfoMove | None:
         for cell in self._sudoku.empty_cells:
             for val in analyzer[cell]:
-                group = None
-                if analyzer.row_occ(cell.row, val) == 1:
-                    group = "row"
-                elif analyzer.col_occ(cell.col, val) == 1:
-                    group = "col"
-                elif analyzer.block_occ(cell.block, val) == 1:
-                    group = "block"
-                if group:
-                    return InfoMove(
-                        cell, val, f"according {group} has only one occurrence"
+                if group := self._get_unique_group(analyzer, cell, val):
+                    return self._move(
+                        cell, val, f"according {group} has only one occurrence", 5
                     )
 
     def _brute_force(self, analyzer: Analyzer) -> Generator[InfoMove]:
         cell = min(self._sudoku.empty_cells, key=lambda cell: len(analyzer[cell]))
         self._complexity += 25 ** len(analyzer[cell])
         for val in analyzer[cell]:
-            self.record("branch-in", InfoMove(cell, val, "brute force"))
-            yield InfoMove(cell, val)
-            self.record("branch-out", InfoMove(cell, val, "no solution"))
-
-    def record(
-        self,
-        mode: Literal[
-            "easy", "medium", "brute force", "branch-in", "branch-out"
-        ],
-        move: InfoMove,
-    ) -> None:
-        if mode == "easy":
-            self._complexity += 1
-        elif mode == "medium":
-            self._complexity += 5
-        if self._explain:
-            print(move)
+            yield self._move(cell, val, "brute force", 0)
 
     def predict_move(self) -> Generator[InfoMove]:
         options = Analyzer(self._sudoku)
         if not options.check():
             ...
         elif move := self._easy_move(options):
-            self.record("easy", move)
             yield move
         elif move := self._medium_move(options):
-            self.record("medium", move)
             yield move
         else:
             yield from self._brute_force(options)
